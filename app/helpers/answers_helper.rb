763e07e050bd5f0a440c27f18f7d41035e160d40
artistpush <artistpush@gmail.com>
2010-05-15 19:46:24 +0800

    dy:修改了整体字体格式

--- app/helpers/answers_helper.rb
+++ app/helpers/answers_helper.rb
@@ -1,14 +1,16 @@
 module AnswersHelper
   def correct_answer(answer)
     html = ""
-    if  answer.question.correct_answer != 0 and answer.question.correct_answer == answer.id
-			return html = "#{image_tag("/images/answer-accepted-on.png", :width => 30)}"
+    if  answer.question.correct_answer != 0
+      if answer.question.correct_answer == answer.id
+        return html = "#{image_tag("/images/answer-accepted-on.png", :width => 30)}"
+      end
 		else
 			if user_id == answer.question.user.id
 				 return html = "#{ link_to "",
 							{ :action => :set_correct_answer, :controller => :questions,
-								:question_id => show_answer.question.id,
-								:answer_id => show_answer.id},
+								:question_id => answer.question.id,
+								:answer_id => answer.id},
 							{ :class => "set_right_answer", :title=>"设为正确答案"} }"
       end
 		end
